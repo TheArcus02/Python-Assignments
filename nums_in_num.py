c28ccd9f085fe275afb227823c694512301cef63
Mikołaj <internetowiec2002@gmail.com>
2022-10-23 21:50:45 +0200

    added nums_in_num working script

---
+++ nums_in_num.py
@@ -0,0 +1,57 @@
+"""
+
+"""
+
+
+def get_possible_nums(nums: str):
+    all_possible_nums = []
+    for i in range(len(nums)):
+        possible_nums = []
+
+        for j in range(len(nums)):
+            if j+i+1 > len(nums):
+                continue
+            possible_num = nums[j:j+i+1]
+            possible_nums.append(int(possible_num))
+        all_possible_nums.append(possible_nums)
+
+    return all_possible_nums
+
+
+def get_most_frequent(nums: list):
+
+    frequency_count = {}
+    maxes = []
+    for num in nums:
+        if num not in frequency_count:
+            frequency_count[num] = 1
+        else:
+            frequency_count[num] += 1
+    for key, val in frequency_count.items():
+        if val is max(frequency_count.values()):
+            maxes.append(key)
+
+    return maxes
+
+
+def get_answers(all_nums: list):
+    answers = []
+
+    for nums in all_nums:
+        most_frequent_nums = get_most_frequent(nums)
+        if len(most_frequent_nums) == 1:
+            answers.append(most_frequent_nums[0])
+        else:
+            answers.append(min(most_frequent_nums))
+    return answers
+
+
+def main():
+    nums = input('')
+    possible_nums = get_possible_nums(nums)
+    answers = get_answers(possible_nums)
+    for answer in answers:
+        print(answer)
+
+    # print(get_most_frequent([1,2,3,3,2]))
+main()
