0362bcd875ba3b8248d8396b0c26a9d431e55439
TheArcus02 <m.urbaniak.inf@gmail.com>
2022-11-29 22:29:23 +0100

    refactored pictures.py (some cases approved)

--- pictures.py
+++ pictures.py
@@ -1,49 +1,41 @@
-def get_possible_widths(wall: tuple[tuple[int]], x: int):
+def can_fit(wall: tuple[tuple[int]],
+            h: int, w: int, y: int, x: int,
+            start: int, end: int,
+            y_streak: int):
 
-    all_streaks = []
+    print(wall, start, end, y_streak)
 
-    for row in wall:
+    for row_idx, row in enumerate(wall):
+        curr_width_streak = 0
+        curr_start_idx = 0
+        print('row', row)
+        for idx, val in enumerate(row[start: end]):
+            print('val', val)
+            if val == 0:
+                if curr_width_streak == 0:
+                    curr_start_idx = idx
+                curr_width_streak += 1
 
-        streaks = []
-        width_streak = 0
-        start_idx = -1
-
-        for idx, col in enumerate(row):
-            if col == 0:
-                if width_streak == 0:
-                    start_idx = idx
-                width_streak += 1
-            if idx == len(row)-1 or col == 1:
-                if width_streak >= x:
-                    streaks.append((width_streak, start_idx, idx - 1 if col == 1 else idx))
-                #     change logic
-                # check if can fit from here
-                #  write a function that checks fields below
-                # if matched return True
-                width_streak = 0
-
-        all_streaks.append(streaks)
-
-    return all_streaks
-
-def can_fit(streaks: list[list[tuple[int, int, int]]], wall: tuple[tuple[int]],
-            y: int):
-    for row_idx, streak in enumerate(streaks):
-        for width_streak in streak:
-
-            if row_idx != len(wall):
-                for row in wall[row_idx+1:]:
-                    next_row = row[width_streak[1]:width_streak[2]+1]
+            if idx == len(row[start:end]) - 1 or val == 1:
+                print('streak', curr_width_streak)
+                if curr_width_streak >= x:
+                    if y_streak + 1 == y:
+                        return True
+                    if can_fit(wall[row_idx + 1:], h, w, y, x,
+                               curr_start_idx,
+                               idx if val == 1 else idx + 1,
+                               y_streak + 1):
+                        return True
+                curr_width_streak = 0
+        y_streak = 0
+    return False
 
 
 def main():
     height, width, y, x = (int(x) for x in input('').split())
 
     wall = tuple(map(lambda idx: tuple(int(i) for i in input('').split()), range(height)))
-    print(wall)
-    streaks = get_possible_widths(wall, x)
-    print(streaks)
-
+    print(can_fit(wall, height, width, y, x, 0, width, 0))
 
 
 if __name__ == '__main__':
